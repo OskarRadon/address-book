70e72de0b9641e0b5081f7951370b444bcd1d653
Blake Scott and Oskar Radon <blakerscott@gmail.com>
2016-01-26 08:58:43 -0800

    add passing specs for full address

--- spec/specs.js
+++ spec/specs.js
@@ -1,20 +1,17 @@
 describe('Contact', function() {
-  it("creates a new contact with the given properties", function() {
-    var testContact = new Contact("Rita", "Moreno");
+
+  it("creates a new contact with the given specifications", function() {
+    var testContact = new Contact("Rita","Moreno");
     expect(testContact.firstName).to.equal("Rita");
     expect(testContact.lastName).to.equal("Moreno");
     expect(testContact.addresses).to.eql([]);
   });
-  it("adds the fullName method to a contact", function() {
-    var testContact = new Contact("Sherlock", "Holmes");
-    expect(testContact.fullName()).to.equal("Sherlock Holmes");
+
+  it("adds the fullName method to all contacts", function() {
+      var testContact = new Contact("Sherlock","Holmes");
+      expect(testContact.fullName()).to.equal("Sherlock Holmes");
   });
-  it("creates a new address with the given specifications", function() {
-    var testAddress = new Address("123 Main St", "Test City","Test State");
-    expect(testAddress.street).to.equal("123 Main St");
-    expect(testAddress.city).to.equal("Test City");
-    expect(testAddress.state).to.equal("Test State");
-  });
+
 });
 
 describe('Address', function() {
@@ -29,6 +26,5 @@ describe('Address', function() {
     var testAddress = new Address("123 Main St", "Test City","Test State");
     expect(testAddress.fullAddress()).to.equal("123 Main St, Test City, Test State");
   });
-
 
 });
